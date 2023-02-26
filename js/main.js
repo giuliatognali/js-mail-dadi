b914db0ee130827671e49ef723f0bc2988259482
giuliatognali <giuliatognali@gmail.com>
2023-02-26 23:57:37 +0100

    valid message

--- js/main.js
+++ js/main.js
@@ -1,26 +1,41 @@
 'use strict';
-
-const checkBtn = document.getElementById('check-btn');
+//definizione e assegnazioni variabili 
 const emailInput = document.getElementById('email');
+const checkBtn = document.getElementById('check-btn');
+const resetBtn = document.getElementById('reset-btn');
 
+//definizione e assegnazioni variabili di messaggio al click
+const validMessage = document.getElementById('valid-email');
+const invalidMessage = document.getElementById('invalid-email');
 
-const emailVerified  = ["test@gmail.com","boolean@gmail.com", "welovehtml@gmail.com", "welovecss@gmail.com", "welovejs@gmail.com"];
+//array email
+const emailVerified = ["giuliatoti@hotmail.it", "test@gmail.com", "boolean@gmail.com", "welovehtml@gmail.com", "welovecss@gmail.com", "welovejs@gmail.com"];
 
 //console.log(emailVerified);
 
 checkBtn.addEventListener('click',
-function(){
-    
-    for(let i = 0; i < emailVerified.length; i ++) {
-        //console.log(i);
-        if (emailVerified[i] === emailInput.value){
-            alert("mail valida");
-            console.log(i);
-            }
-        else{
-            alert("mail NON valida");
-        }
-    }
-})
+    function () {
 
-console.log(emailInput.value);
+        for (let i = 0; i < emailVerified.length; i++) {
+
+            const emailInputValue = emailInput.value
+
+            if (emailVerified[i] === emailInputValue) {
+                validMessage.classList.add('show');
+                break;
+
+                //inserire da mostrare il div con il gioco
+            }
+            else if (emailVerified[i] !== emailInputValue) {
+                invalidMessage.classList.add('show');
+            }
+        }
+    })
+
+resetBtn.addEventListener('click',
+function() {
+
+    validMessage.classList.remove('show');
+    invalidMessage.classList.remove('show');
+    emailInput.value = "";
+})
